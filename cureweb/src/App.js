067b314a93f2a5c0a771a0e79213c24eb3472ff8
karmpatel960 <96860884+Karmpatel960@users.noreply.github.com>
2023-03-22 00:31:25 +0530

    nav logo added

--- cureweb/src/App.js
+++ cureweb/src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+<img src="/CureML__1_-removebg-preview.png" alt="Example image" />
+<a>Home</a>
+<a>feacture</a>
+<a>About Us</a>
+<a>Contect</a>
+<nav>
+          <a>Login</a>
+          <button>Sign In</button>
+        </nav>
       </header>
+
+      
     </div>
   );
 }
